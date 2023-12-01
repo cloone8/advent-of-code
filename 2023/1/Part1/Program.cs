1712b53d0c32d07ad6643e0aa6d5368e3695deb4
Wouter de Bruijn <wouter@wutru.io>
2023-12-01 11:54:16 +0100

    Day one 2023

---
+++ 2023/1/Part1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+
+class MainClass {
+    public static void Main(string[] args) {
+        int result = File
+            .ReadLines("input.txt")
+            .Select(GetCalibrationValue)
+            .Sum();
+
+        Console.WriteLine(result);
+    }
+
+    private static int GetCalibrationValue(string line) {
+        List<char> onlyDigits = line.Where(c => c >= '0' && c <= '9').ToList();
+
+        if(onlyDigits.Count == 0) {
+            return 0;
+        }
+
+        string firstLast = onlyDigits.First().ToString() + onlyDigits.Last().ToString();
+
+        return int.Parse(firstLast);
+    }
+}
---
+++ 2023/1/Part2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+
+class MainClass {
+    private static Dictionary<string, char> digitsAsStrings = new Dictionary<string, char> {
+        {"one", '1'},
+        {"two", '2'},
+        {"three", '3'},
+        {"four", '4'},
+        {"five", '5'},
+        {"six", '6'},
+        {"seven", '7'},
+        {"eight", '8'},
+        {"nine", '9'},
+    };
+
+    public static void Main(string[] args) {
+        int result = File
+            .ReadLines("input.txt")
+            .Select(GetCalibrationValue)
+            .Sum();
+
+        Console.WriteLine(result);
+    }
+
+    private static void SetDigits(ref char firstDigit, ref char lastDigit, char newDigit) {
+        if(firstDigit == '\0') {
+            firstDigit = newDigit;
+        }
+
+        lastDigit = newDigit;
+    }
+
+    private static int GetCalibrationValue(string line) {
+        char firstDigit = '\0';
+        char lastDigit = '\0';
+
+        for(int i = 0; i < line.Length; i++) {
+            if(line[i] >= '0' && line[i] <= '9') {
+                SetDigits(ref firstDigit, ref lastDigit, line[i]);
+                continue;
+            }
+
+            foreach(KeyValuePair<string, char> digit in digitsAsStrings) {
+                if(line[i..].StartsWith(digit.Key)) {
+                    SetDigits(ref firstDigit, ref lastDigit, digit.Value);
+                    break;
+                }
+            }
+        }
+
+        return int.Parse(firstDigit.ToString() + lastDigit.ToString());
+    }
+}
