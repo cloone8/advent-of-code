443bcb665d21ea6b50aac88c55664362c3c64de7
Wouter de Bruijn <wouter@wutru.io>
2023-12-04 16:11:26 +0100

    Day 4

---
+++ 2023/4/Part1/MainClass.cs
@@ -0,0 +1,46 @@
+﻿class MainClass {
+    public static void Main(string[] args) {
+        int sum = File
+            .ReadLines("input.txt")
+            .Select(ParseCard)
+            .Select(GetPoints)
+            .Sum();
+
+        Console.WriteLine(sum);
+    }
+
+    private static Card ParseCard(string line) {
+        string noId = line.Split(':')[1].Trim();
+        string[] numbers = noId.Split('|');
+        string winningNumbers = numbers[0].Trim();
+        string yourNumbers = numbers[1].Trim();
+        List<int> winningNumbersList = winningNumbers.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        List<int> yourNumbersList = yourNumbers.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        return new Card() {
+            WinningNumbers = winningNumbersList,
+            YourNumbers = yourNumbersList
+        };
+    }
+
+    private static int GetPoints(Card card) {
+        int points = 0;
+        foreach (int number in card.YourNumbers) {
+            if (card.WinningNumbers.Contains(number)) {
+                if(points == 0) {
+                    points = 1;
+                } else {
+                    points *= 2;
+                }
+            }
+        }
+        return points;
+    }
+}
+
+struct Card {
+    public List<int> WinningNumbers = [];
+    public List<int> YourNumbers = [];
+
+    public Card() {}
+}
---
+++ 2023/4/Part2/MainClass.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+
+class MainClass {
+    private static List<Card> cards = [];
+
+    public static void Main(string[] args) {
+        cards = File
+            .ReadLines("input.txt")
+            .Select(ParseCard)
+            .ToList();
+
+        for(int i = 0; i < cards.Count; i++) {
+            int matching = GetNumMatching(cards[i]);
+
+            for(int j = 1; j < cards.Count && j <= matching; j++) {
+                cards[i + j].Copies += cards[i].Copies;
+            }
+        }
+
+        int sum = cards
+            .Select(c => c.Copies)
+            .Sum();
+
+        Console.WriteLine(sum);
+    }
+
+    private static Card ParseCard(string line) {
+        string noId = line.Split(':')[1].Trim();
+        string[] numbers = noId.Split('|');
+        string winningNumbers = numbers[0].Trim();
+        string yourNumbers = numbers[1].Trim();
+        List<int> winningNumbersList = winningNumbers.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        List<int> yourNumbersList = yourNumbers.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        return new Card() {
+            WinningNumbers = winningNumbersList,
+            YourNumbers = yourNumbersList,
+            Copies = 1
+        };
+    }
+
+    private static int GetNumMatching(Card card) {
+        int matching = 0;
+
+        foreach (int number in card.YourNumbers) {
+            if (card.WinningNumbers.Contains(number)) {
+                matching++;
+            }
+        }
+
+        return matching;
+    }
+}
+
+class Card {
+    public int Copies = 1;
+    public List<int> WinningNumbers = [];
+    public List<int> YourNumbers = [];
+
+    public Card() {}
+}
